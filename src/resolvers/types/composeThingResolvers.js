d968a9d135410822066c7ef09dfdcfffb4b60288
Valentyn Soloviov <v.solovyov@intellect.ua>
2019-04-16 18:22:57 +0300

    fix(composeThingResolvers): Add empedded fields processing

--- src/resolvers/types/composeThingResolvers.js
+++ src/resolvers/types/composeThingResolvers.js
@@ -8,7 +8,7 @@ const createThingScalarResolver = require('./createThingScalarResolver');
 type ThingResolver = { [key: string]: Function };
 
 const composeThingResolvers = (thingConfig: ThingConfig): ThingResolver => {
-  const { relationalFields } = thingConfig;
+  const { embeddedFields, relationalFields } = thingConfig;
 
   if (!relationalFields)
     throw new TypeError('Expected an array as a value of the relationalFields key of thingConfig');
@@ -25,6 +25,29 @@ const composeThingResolvers = (thingConfig: ThingConfig): ThingResolver => {
     }
     return prev;
   }, {});
+
+  if (embeddedFields) {
+    embeddedFields.reduce((prev, { name, config: { relationalFields: relationalFields2 } }) => {
+      const childeren = {};
+      if (relationalFields2) {
+        relationalFields2.reduce((prev2, { array, name: name2, config }) => {
+          if (array) {
+            const resolver = createThingArrayResolver(config);
+            // eslint-disable-next-line no-param-reassign
+            prev2[name2] = resolver;
+          } else {
+            const resolver = createThingScalarResolver(config);
+            // eslint-disable-next-line no-param-reassign
+            prev2[name2] = resolver;
+          }
+          return prev2;
+        }, childeren);
+      }
+      // eslint-disable-next-line no-param-reassign
+      if (Object.keys(childeren).length) prev[name] = childeren;
+      return prev;
+    }, resolvers);
+  }
 
   return resolvers;
 };
