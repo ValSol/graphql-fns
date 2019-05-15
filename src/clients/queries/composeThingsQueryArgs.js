b55db96e8c5c46befd884cc461db03f546e8daf5
Valentyn Soloviov <v.solovyov@intellect.ua>
2019-05-15 18:38:10 +0300

    feat(composeThingsQueryArgs): Create util to compose things query args

---
+++ src/clients/queries/composeThingsQueryArgs.js
@@ -0,0 +1,45 @@
+// @flow
+import type { ThingConfig } from '../../flowTypes';
+
+const pluralize = require('pluralize');
+
+const createThingNearInputType = require('../../types/inputs/createThingNearInputType');
+const createThingPaginationInputType = require('../../types/inputs/createThingPaginationInputType');
+const createThingSortInputType = require('../../types/inputs/createThingSortInputType');
+const createThingWhereInputType = require('../../types/inputs/createThingWhereInputType');
+
+const composeThingQueryArgs = (thingConfig: ThingConfig): Array<string> => {
+  const { name } = thingConfig;
+  const argsArray = [];
+
+  const thingNearInputType = createThingNearInputType(thingConfig);
+  if (thingNearInputType) {
+    argsArray.push({ argName: 'near', argType: `${name}NearInput` });
+  }
+
+  const thingPaginationInputType = createThingPaginationInputType(thingConfig);
+  if (thingPaginationInputType) {
+    argsArray.push({ argName: 'pagination', argType: `${name}PaginationInput` });
+  }
+
+  const tThingWhereInputType = createThingWhereInputType(thingConfig);
+  if (tThingWhereInputType) {
+    argsArray.push({ argName: 'where', argType: `${name}WhereInput` });
+  }
+
+  const thingSortInputType = createThingSortInputType(thingConfig);
+  if (thingSortInputType) {
+    argsArray.push({ argName: 'sort', argType: `${name}SortInput` });
+  }
+
+  if (!argsArray.length) {
+    return [`query ${pluralize(name)} {`, `  ${pluralize(name)} {`];
+  }
+
+  const args1 = argsArray.map(({ argName, argType }) => `$${argName}: ${argType}`).join(', ');
+  const args2 = argsArray.map(({ argName }) => `${argName}: $${argName}`).join(', ');
+
+  return [`query ${pluralize(name)}(${args1}) {`, `  ${pluralize(name)}(${args2}) {`];
+};
+
+module.exports = composeThingQueryArgs;
---
+++ src/clients/queries/composeThingsQueryArgs.test.js
@@ -0,0 +1,84 @@
+// @flow
+/* eslint-env jest */
+
+import type { ThingConfig } from '../../flowTypes';
+
+const composeThingsQueryArgs = require('./composeThingsQueryArgs');
+
+describe('composeThingsQueryArgs', () => {
+  test('should compose things query without args', () => {
+    const thingConfig: ThingConfig = {
+      name: 'Example',
+      textFields: [
+        {
+          name: 'textField',
+        },
+      ],
+    };
+
+    const expectedResult = ['query Examples {', '  Examples {'];
+
+    const result = composeThingsQueryArgs(thingConfig);
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('should compose things query with ExampleWhereInput and ExampleSortInput args', () => {
+    const thingConfig: ThingConfig = {
+      name: 'Example',
+      textFields: [
+        {
+          name: 'textField',
+          index: true,
+        },
+      ],
+    };
+
+    const expectedResult = [
+      'query Examples($where: ExampleWhereInput, $sort: ExampleSortInput) {',
+      '  Examples(where: $where, sort: $sort) {',
+    ];
+
+    const result = composeThingsQueryArgs(thingConfig);
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('should compose things query with ExamplePaginationInput args', () => {
+    const thingConfig: ThingConfig = {
+      name: 'Example',
+      pagination: true,
+      textFields: [
+        {
+          name: 'textField',
+        },
+      ],
+    };
+
+    const expectedResult = [
+      'query Examples($pagination: ExamplePaginationInput) {',
+      '  Examples(pagination: $pagination) {',
+    ];
+
+    const result = composeThingsQueryArgs(thingConfig);
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('should compose things query with ExampleNearInput args', () => {
+    const thingConfig: ThingConfig = {
+      name: 'Example',
+      geospatialFields: [
+        {
+          name: 'position',
+          type: 'Point',
+        },
+      ],
+    };
+
+    const expectedResult = [
+      'query Examples($near: ExampleNearInput) {',
+      '  Examples(near: $near) {',
+    ];
+
+    const result = composeThingsQueryArgs(thingConfig);
+    expect(result).toEqual(expectedResult);
+  });
+});
