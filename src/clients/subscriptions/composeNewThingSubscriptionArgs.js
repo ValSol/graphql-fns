472536295c10f27009cdce6b6c23d402d4d2c51d
Valentyn Soloviov <v.solovyov@intellect.ua>
2019-05-16 08:45:55 +0300

    feat(composeNewThingSubscriptionArgs): Create util to compose newThing subscription

---
+++ src/clients/subscriptions/composeNewThingSubscriptionArgs.js
@@ -0,0 +1,26 @@
+// @flow
+import type { ThingConfig } from '../../flowTypes';
+
+const createThingWhereInputType = require('../../types/inputs/createThingWhereInputType');
+
+const composeNewThingSubscriptionArgs = (thingConfig: ThingConfig): Array<string> => {
+  const { name } = thingConfig;
+
+  const argsArray = [];
+
+  const tThingWhereInputType = createThingWhereInputType(thingConfig);
+  if (tThingWhereInputType) {
+    argsArray.push({ argName: 'where', argType: `${name}WhereInput` });
+  }
+
+  if (!argsArray.length) {
+    return [`subscription new${name} {`, `  new${name} {`];
+  }
+
+  const args1 = argsArray.map(({ argName, argType }) => `$${argName}: ${argType}`).join(', ');
+  const args2 = argsArray.map(({ argName }) => `${argName}: $${argName}`).join(', ');
+
+  return [`subscription new${name}(${args1}) {`, `  new${name}(${args2}) {`];
+};
+
+module.exports = composeNewThingSubscriptionArgs;
---
+++ src/clients/subscriptions/composeNewThingSubscriptionArgs.test.js
@@ -0,0 +1,44 @@
+// @flow
+/* eslint-env jest */
+
+import type { ThingConfig } from '../../flowTypes';
+
+const composeNewThingSubscriptionArgs = require('./composeNewThingSubscriptionArgs');
+
+describe('composeNewThingSubscriptionArgs', () => {
+  test('should compose things query without args', () => {
+    const thingConfig: ThingConfig = {
+      name: 'Example',
+      textFields: [
+        {
+          name: 'textField',
+        },
+      ],
+    };
+
+    const expectedResult = ['subscription newExample {', '  newExample {'];
+
+    const result = composeNewThingSubscriptionArgs(thingConfig);
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('should compose things query with ExampleWhereInput arg', () => {
+    const thingConfig: ThingConfig = {
+      name: 'Example',
+      textFields: [
+        {
+          name: 'textField',
+          index: true,
+        },
+      ],
+    };
+
+    const expectedResult = [
+      'subscription newExample($where: ExampleWhereInput) {',
+      '  newExample(where: $where) {',
+    ];
+
+    const result = composeNewThingSubscriptionArgs(thingConfig);
+    expect(result).toEqual(expectedResult);
+  });
+});
