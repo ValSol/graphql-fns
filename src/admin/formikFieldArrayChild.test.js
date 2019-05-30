2d246133a84f184bdd2d3d9d05c13d4b5f30e162
Valentyn Soloviov <v.solovyov@intellect.ua>
2019-05-30 10:32:38 +0300

    feat(src/admin/formikFieldArrayChild.js): Update to exchange Button by IconButton

---
+++ src/admin/formikFieldArrayChild.test.js
@@ -0,0 +1,98 @@
+// @flow
+/* eslint-env jest */
+
+import React from 'react';
+import { Field } from 'formik';
+import { TextField as FormikTextField } from 'formik-material-ui';
+import Tooltip from '@material-ui/core/Tooltip';
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
+import InputAdornment from '@material-ui/core/InputAdornment';
+
+import formikFieldArrayChild from './formikFieldArrayChild';
+
+describe('composeFormikFieldArrayChild', () => {
+  test('should return fragment for a simple fields set', () => {
+    const values = {
+      people: {
+        friends: [{ name: 'Vasya', emails: ['vasya@gmail.com', 'vasya@ukr.net'] }],
+      },
+    };
+    const name = 'people.friends[0].emails';
+    const expectedResult = (
+      <React.Fragment>
+        {[
+          <Field
+            key={0}
+            component={FormikTextField}
+            fullWidth
+            label="email #1"
+            margin="normal"
+            name="people.friends[0].emails[0]"
+            variant="outlined"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Tooltip title="Delete email #1" placement="left">
+                    <IconButton
+                      aria-label="Delete email #1"
+                      disabled={false}
+                      edge="end"
+                      onClick={() => {}}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  </Tooltip>
+                </InputAdornment>
+              ),
+            }}
+          />,
+          <Field
+            key={1}
+            component={FormikTextField}
+            fullWidth
+            label="email #2"
+            margin="normal"
+            name="people.friends[0].emails[1]"
+            variant="outlined"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Tooltip title="Delete email #2" placement="left">
+                    <IconButton
+                      aria-label="Delete email #2"
+                      disabled={false}
+                      edge="end"
+                      onClick={() => {}}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  </Tooltip>
+                </InputAdornment>
+              ),
+            }}
+          />,
+        ]}
+        <Tooltip title="Add email" placement="right">
+          <IconButton aria-label="Add email" onClick={() => {}} disabled={false}>
+            <AddIcon />
+          </IconButton>
+        </Tooltip>
+      </React.Fragment>
+    );
+
+    const props = {
+      form: { values, isSubmitting: false },
+      name,
+      push() {},
+      remove() {},
+    };
+    const result = formikFieldArrayChild(props);
+
+    const result2 = JSON.parse(JSON.stringify(result));
+    const expectedResult2 = JSON.parse(JSON.stringify(expectedResult));
+
+    expect(result2).toEqual(expectedResult2);
+  });
+});
