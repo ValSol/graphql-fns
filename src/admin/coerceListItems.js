590630851b1cfb9f247ad2a6d65b7b702f03201f
Valentyn Soloviov <v.solovyov@intellect.ua>
2019-06-04 17:09:07 +0300

    feat(src/admin/coerceListItems.js): Create util to coerce list items

---
+++ src/admin/coerceListItems.js
@@ -0,0 +1,132 @@
+// @flow
+import type { ThingConfig, ListColumn } from '../flowTypes';
+
+import composeFieldsObject from './composeFieldsObject';
+
+const coerceListItems = (items: Object, thingConfig: ThingConfig): Array<ListColumn> => {
+  const fieldsObject = composeFieldsObject(thingConfig);
+
+  return items.map(item =>
+    Object.keys(item).reduce((prev, key) => {
+      if (key === '__typename') return prev;
+
+      if (['id', 'createdAt', 'updatedAt'].includes(key)) {
+        prev[key] = item[key]; // eslint-disable-line no-param-reassign
+        return prev;
+      }
+
+      const { kind, array, geospatialType } = fieldsObject[key];
+      if (array && !item[key].length) {
+        prev[key] = ''; // eslint-disable-line no-param-reassign
+        return prev;
+      }
+
+      if (item[key] === undefined || item[key] === null) {
+        prev[key] = ''; // eslint-disable-line no-param-reassign
+        return prev;
+      }
+      switch (kind) {
+        case 'textFields':
+          if (array) {
+            prev[key] = item[key].join(', '); // eslint-disable-line no-param-reassign
+          } else {
+            prev[key] = item[key]; // eslint-disable-line no-param-reassign
+          }
+          break;
+
+        case 'booleanFields':
+          if (array) {
+            // eslint-disable-next-line no-param-reassign
+            prev[key] = item[key].map(value => (value ? 'true' : 'false')).join(', ');
+          } else {
+            prev[key] = item[key] ? 'true' : 'false'; // eslint-disable-line no-param-reassign
+          }
+          break;
+
+        case 'intFields':
+          if (array) {
+            // eslint-disable-next-line no-param-reassign
+            prev[key] = item[key].join(', ');
+          } else {
+            prev[key] = item[key]; // eslint-disable-line no-param-reassign
+          }
+          break;
+
+        case 'floatFields':
+          if (array) {
+            // eslint-disable-next-line no-param-reassign
+            prev[key] = item[key].join(', ');
+          } else {
+            prev[key] = item[key]; // eslint-disable-line no-param-reassign
+          }
+          break;
+
+        case 'enumFields':
+          if (array) {
+            prev[key] = item[key].join(', '); // eslint-disable-line no-param-reassign
+          } else {
+            prev[key] = item[key]; // eslint-disable-line no-param-reassign
+          }
+          break;
+
+        case 'dateTimeFields':
+          if (array) {
+            prev[key] = item[key].join(', '); // eslint-disable-line no-param-reassign
+          } else {
+            prev[key] = item[key]; // eslint-disable-line no-param-reassign
+          }
+          break;
+
+        case 'geospatialFields':
+          if (geospatialType === 'Point') {
+            if (array) {
+              // eslint-disable-next-line no-param-reassign
+              prev[key] = item[key]
+                .map(({ longitude, latitude }) => `(${longitude}, ${latitude})`)
+                .join(', ');
+            } else {
+              const { longitude, latitude } = item[key];
+              prev[key] = `(${longitude}, ${latitude})`; // eslint-disable-line no-param-reassign
+            }
+          } else if (array) {
+            // eslint-disable-next-line no-param-reassign
+            prev[key] = item[key].map(() => 'polygon').join(', ');
+          } else {
+            prev[key] = 'polygon'; // eslint-disable-line no-param-reassign
+          }
+          break;
+
+        case 'duplexFields':
+          if (array) {
+            prev[key] = item[key].join(', '); // eslint-disable-line no-param-reassign
+          } else {
+            prev[key] = item[key]; // eslint-disable-line no-param-reassign
+          }
+          break;
+
+        case 'relationalFields':
+          if (array) {
+            prev[key] = item[key].join(', '); // eslint-disable-line no-param-reassign
+          } else {
+            prev[key] = item[key]; // eslint-disable-line no-param-reassign
+          }
+          break;
+
+        case 'embeddedFields':
+          if (array) {
+            // eslint-disable-next-line no-param-reassign
+            prev[key] = item[key].map(() => 'embedded').join(', ');
+          } else {
+            prev[key] = 'embedded'; // eslint-disable-line no-param-reassign
+          }
+          break;
+
+        default:
+          break;
+      }
+      return prev;
+    }, {}),
+  );
+};
+
+export default coerceListItems;
---
+++ src/admin/flowTypes.js
@@ -0,0 +1,239 @@
+// @flow
+
+export type MongodbGeospatialPoint = {|
+  type: 'Point',
+  coordinates: [number, number],
+|};
+
+export type MongodbGeospatialPolygon = {|
+  type: 'Polygon',
+  coordinates: Array<Array<[number, number]>>,
+|};
+
+export type GeospatialPoint = {|
+  longitude: number,
+  latitude: number,
+|};
+
+export type GeospatialPolygon = {
+  externalRing: {| ring: Array<GeospatialPoint> |},
+  internalRings?: Array<{ ring: Array<GeospatialPoint> }>,
+};
+
+type BooleanField = {
+  name: string,
+  array?: boolean,
+  default?: boolean | Array<boolean>,
+  index?: boolean,
+  required?: boolean,
+};
+
+type EnumField = {
+  name: string,
+  array?: boolean,
+  default?: string | Array<string>,
+  index?: boolean,
+  required?: boolean,
+  enumName: string, // name to compose graphql types
+};
+
+type GeospatialField = {
+  name: string,
+  required?: boolean,
+  geospatialType: 'Point' | 'Polygon',
+  array?: boolean,
+};
+
+type ScalarInterfaceField = {
+  array?: boolean,
+  index?: boolean,
+  required?: boolean,
+  unique?: boolean,
+};
+
+type TextField = {
+  ...ScalarInterfaceField,
+  name: string,
+  default?: string | Array<string>,
+};
+
+type DateTimeField = {
+  ...ScalarInterfaceField,
+  name: string,
+  default?: Date | Array<Date>,
+};
+
+type IntField = {
+  ...ScalarInterfaceField,
+  name: string,
+  default?: number | Array<number>,
+};
+
+type FloatField = {
+  ...ScalarInterfaceField,
+  name: string,
+  default?: number | Array<number>,
+};
+
+export type FormField = {
+  name: string,
+  formFieldType?: 'hidden' | 'disabled' | 'email' | 'multiline',
+};
+
+export type ListColumn = {
+  name: string,
+  width: number, // pixels
+};
+
+export type ThingConfig = {
+  name: string,
+  embedded?: boolean,
+  pagination?: boolean,
+
+  duplexFields?: Array<{
+    name: string,
+    array?: boolean,
+    config: ThingConfig,
+    index?: boolean,
+    oppositeName: string,
+    required?: boolean,
+  }>,
+  embeddedFields?: Array<{
+    name: string,
+    required?: boolean,
+    array?: boolean,
+    config: ThingConfig,
+  }>,
+  relationalFields?: Array<{
+    name: string,
+    array?: boolean,
+    config: ThingConfig,
+    index?: boolean,
+    required?: boolean,
+  }>,
+
+  booleanFields?: Array<BooleanField>,
+  dateTimeFields?: Array<DateTimeField>,
+  enumFields?: Array<EnumField>,
+  geospatialFields?: Array<GeospatialField>,
+  intFields?: Array<IntField>,
+  floatFields?: Array<FloatField>,
+  textFields?: Array<TextField>,
+
+  form?: Array<FormField>,
+  list?: Array<ListColumn>,
+};
+
+export type ThingConfigObject = {
+  [fieldName: string]: {
+    [property: string]: any,
+    name: string,
+    kind:
+      | 'booleanFields'
+      | 'dateTimeFields'
+      | 'duplexFields'
+      | 'embeddedFields'
+      | 'enumFields'
+      | 'floatFields'
+      | 'geospatialFields'
+      | 'intFields'
+      | 'relationalFields'
+      | 'textFields',
+  },
+};
+
+export type FlatFormikFields = Array<{
+  name: string,
+  array?: boolean,
+  child?: FlatFormikFields,
+  config?: ThingConfig,
+}>;
+
+export type Enums = Array<{ name: string, enum: Array<string> }>;
+
+type thingNamesList = null | Array<string>;
+
+type InverntoryOptions = {
+  Query?: null | {
+    thing?: thingNamesList,
+    things?: thingNamesList,
+    thingCount?: thingNamesList,
+  },
+  Mutation?: null | {
+    createThing?: thingNamesList,
+    updateThing?: thingNamesList,
+    deleteThing?: thingNamesList,
+  },
+  Subscription?: null | {
+    newThing?: thingNamesList,
+  },
+};
+export type Inventory = {
+  include?: null | InverntoryOptions,
+  exclude?: null | InverntoryOptions,
+};
+
+export type GeneralConfig = {
+  thingConfigs: Array<ThingConfig>,
+  enums?: Enums,
+  inventory?: Inventory,
+};
+
+type OneSegmentInventoryChain = ['Query'] | ['Mutation'] | ['Subscription'];
+type TwoSegmentInventoryChain =
+  | ['Query', 'thing' | 'things' | 'thingCount']
+  | ['Mutation', 'createThing' | 'updateThing' | 'deleteThing']
+  | ['Subscription', 'newThing'];
+type ThreeSegmentInventoryChain =
+  | ['Query', 'thing' | 'things' | 'thingCount', string]
+  | ['Mutation', 'createThing' | 'updateThing' | 'deleteThing', string]
+  | ['Subscription', 'newThing' | 'updatedThing' | 'deletedThing', string];
+export type InventoryСhain =
+  | OneSegmentInventoryChain
+  | TwoSegmentInventoryChain
+  | ThreeSegmentInventoryChain;
+
+export type Periphery = Map<
+  ThingConfig,
+  {
+    [oppositeName: string]: {
+      oppositeIds: Array<string>,
+      array: boolean,
+      name: string,
+      oppositeConfig: ThingConfig,
+    },
+  },
+>;
+
+export type NearInput = {|
+  geospatialField: string,
+  coordinates: GeospatialPoint,
+  maxDistance: number,
+|};
+
+export type NearMongodb = {
+  // as key using [fieldName].coordinates
+  [key: string]: {
+    $nearSphere: {
+      $geometry: MongodbGeospatialPoint,
+      $maxDistance: number,
+    },
+  },
+};
+
+export type Subscribe = {
+  subscribe: (_: Object, { name: string }, { pubsub: Object }) => Object,
+};
+
+export type ClientOptions = {
+  depth?: number,
+  include?: Object,
+  exclude?: Object,
+};
+
+export type ClientFieldsOptions = {
+  ...ClientOptions,
+  shift: number,
+};
+
+export type RouterQuery = { id?: string, create?: '', thing: string };
