bd8d0e019d0f15155ce158935483cd8353711ae6
Valentyn Soloviov <v.solovyov@intellect.ua>
2019-06-06 12:12:55 +0300

    feat(src/admin/components/AllThings.js): Update to use Breadcrumbs

--- src/admin/components/AllThings.js
+++ src/admin/components/AllThings.js
@@ -2,9 +2,12 @@
 
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Typography from '@material-ui/core/Typography';
 
 import type { GeneralConfig } from '../../flowTypes';
 
+import Link from './Link';
 import ThingCard from './ThingCard';
 
 type Props = { generalConfig: GeneralConfig, router: Object };
@@ -19,6 +22,10 @@ const AllThings = (props: Props) => {
   return (
     <Container>
       <h1>All things</h1>
+      <Breadcrumbs aria-label="Breadcrumb">
+        <Link href="/">Home</Link>
+        <Typography color="textPrimary">All Things</Typography>
+      </Breadcrumbs>
       {thingConfigs
         .filter(({ embedded }) => !embedded)
         .map(config => (
