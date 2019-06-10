ddf19e1b211dbfa36ef00c7e05f2d67395eecfa8
Valentyn Soloviov <v.solovyov@intellect.ua>
2019-06-10 14:17:30 +0300

    refactor(src/admin/composeInitialValues.js): Update to eliminate switch with each field kind

--- src/admin/composeInitialValues.js
+++ src/admin/composeInitialValues.js
@@ -12,37 +12,18 @@ const composeInitialValues = (thingConfig: ThingConfig, data?: Object = {}): Obj
   const result = formFields.reduce((prev, { name }) => {
     const { array, config, default: defaultValue, kind } = fieldsObject[name];
 
-    switch (kind) {
-      case 'embeddedFields':
-        prev[name] = array // eslint-disable-line no-param-reassign, no-nested-ternary
-          ? data[name]
-            ? data[name].map(dataItem => composeInitialValues(config, dataItem))
-            : []
-          : composeInitialValues(config, data[name]); // eslint-disable-line no-param-reassign
-        break;
-      case 'textFields':
-        // eslint-disable-next-line no-param-reassign
-        prev[name] = data[name] !== undefined ? data[name] : defaultValue || (array ? [] : '');
-        break;
-      case 'intFields':
-        // eslint-disable-next-line no-param-reassign
-        prev[name] = data[name] !== undefined ? data[name] : defaultValue || (array ? [] : '');
-        break;
-      case 'floatFields':
-        // eslint-disable-next-line no-param-reassign
-        prev[name] = data[name] !== undefined ? data[name] : defaultValue || (array ? [] : '');
-        break;
-      case 'booleanFields':
-        // eslint-disable-next-line no-param-reassign
-        prev[name] = data[name] !== undefined ? data[name] : defaultValue || (array ? [] : false);
-        break;
-      case 'enumFields':
-        // eslint-disable-next-line no-param-reassign
-        prev[name] = data[name] !== undefined ? data[name] : defaultValue || (array ? [] : '');
-        break;
-
-      default:
-        throw new TypeError(`Invalid kind: "${kind}" of thing field!`);
+    if (kind === 'embeddedFields') {
+      prev[name] = array // eslint-disable-line no-param-reassign, no-nested-ternary
+        ? data[name]
+          ? data[name].map(dataItem => composeInitialValues(config, dataItem))
+          : []
+        : composeInitialValues(config, data[name]); // eslint-disable-line no-param-reassign
+    } else if (kind === 'booleanFields') {
+      // eslint-disable-next-line no-param-reassign
+      prev[name] = data[name] !== undefined ? data[name] : defaultValue || (array ? [] : false);
+    } else {
+      // eslint-disable-next-line no-param-reassign
+      prev[name] = data[name] !== undefined ? data[name] : defaultValue || (array ? [] : '');
     }
 
     return prev;
