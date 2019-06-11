c912ea37248200ce83144376c04d411b806bf218
Valentyn Soloviov <v.solovyov@intellect.ua>
2019-06-11 13:51:02 +0300

    feat(src/resolvers/mutations/clearUpdateInputDate.js): Create util to clear update input date

---
+++ src/resolvers/mutations/clearUpdateInputDate.js
@@ -0,0 +1,26 @@
+// @flow
+
+import type { ThingConfig } from '../../flowTypes';
+
+import composeFieldsObject from '../../utils/composeFieldsObject';
+
+const clearUpdateInputDate = (data: Object, thingConfig: ThingConfig): Object => {
+  const fieldsObject = composeFieldsObject(thingConfig);
+
+  const result = Object.keys(data).reduce((prev, key) => {
+    if (fieldsObject[key] === undefined) return prev;
+    const { config, kind } = fieldsObject[key];
+    if (kind === 'embeddedFields') {
+      prev[key] = clearUpdateInputDate(data[key], config); // eslint-disable-line no-param-reassign
+    } else if (kind === 'duplexFields' || kind === 'relationalFields') {
+      prev[key] = data[key].connect; // eslint-disable-line no-param-reassign
+    } else {
+      prev[key] = data[key]; // eslint-disable-line no-param-reassign
+    }
+    return prev;
+  }, {});
+
+  return result;
+};
+
+module.exports = clearUpdateInputDate;
