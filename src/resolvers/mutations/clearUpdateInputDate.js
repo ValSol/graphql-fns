6c78a2237e36d0059dd23ae03d42e92de05ac768
Valentyn Soloviov <v.solovyov@intellect.ua>
2019-06-13 17:04:18 +0300

    fix(src/resolvers/mutations/clearUpdateInputDate.js): Update to process data with undefined property

--- src/resolvers/mutations/clearUpdateInputDate.js
+++ src/resolvers/mutations/clearUpdateInputDate.js
@@ -8,10 +8,12 @@ const clearUpdateInputDate = (data: Object, thingConfig: ThingConfig): Object =>
   const fieldsObject = composeFieldsObject(thingConfig);
 
   const result = Object.keys(data).reduce((prev, key) => {
-    if (fieldsObject[key] === undefined) return prev;
+    if (fieldsObject[key] === undefined || data[key] === undefined) return prev;
+
     const { config, kind } = fieldsObject[key];
+
     if (kind === 'embeddedFields') {
-      prev[key] = clearUpdateInputDate(data[key], config); // eslint-disable-line no-param-reassign
+      prev[key] = data[key] === null ? null : clearUpdateInputDate(data[key], config); // eslint-disable-line no-param-reassign
     } else if (kind === 'duplexFields' || kind === 'relationalFields') {
       prev[key] = data[key].connect; // eslint-disable-line no-param-reassign
     } else {
