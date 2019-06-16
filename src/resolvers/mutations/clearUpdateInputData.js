60088eb41859931d7c1da24a3fd9a7955fb62adc
Valentyn Soloviov <v.solovyov@intellect.ua>
2019-06-16 17:56:53 +0300

    fix(src/resolvers/mutations/clearUpdateInputData.js): Update to process array of embedded fields

--- src/resolvers/mutations/clearUpdateInputData.js
+++ src/resolvers/mutations/clearUpdateInputData.js
@@ -4,16 +4,20 @@ import type { ThingConfig } from '../../flowTypes';
 
 import composeFieldsObject from '../../utils/composeFieldsObject';
 
-const clearUpdateInputDate = (data: Object, thingConfig: ThingConfig): Object => {
+const clearUpdateInputData = (data: Object, thingConfig: ThingConfig): Object => {
   const fieldsObject = composeFieldsObject(thingConfig);
 
   const result = Object.keys(data).reduce((prev, key) => {
     if (fieldsObject[key] === undefined || data[key] === undefined) return prev;
 
-    const { config, kind } = fieldsObject[key];
+    const { array, config, kind } = fieldsObject[key];
 
     if (kind === 'embeddedFields') {
-      prev[key] = data[key] === null ? null : clearUpdateInputDate(data[key], config); // eslint-disable-line no-param-reassign
+      if (array) {
+        prev[key] = data[key].map(item => clearUpdateInputData(item, config)); // eslint-disable-line no-param-reassign
+      } else {
+        prev[key] = clearUpdateInputData(data[key], config); // eslint-disable-line no-param-reassign
+      }
     } else if (kind === 'duplexFields' || kind === 'relationalFields') {
       prev[key] = data[key].connect; // eslint-disable-line no-param-reassign
     } else {
@@ -25,4 +29,4 @@ const clearUpdateInputDate = (data: Object, thingConfig: ThingConfig): Object =>
   return result;
 };
 
-module.exports = clearUpdateInputDate;
+module.exports = clearUpdateInputData;
---
+++ src/resolvers/mutations/clearUpdateInputData.test.js
@@ -0,0 +1,97 @@
+// @flow
+/* eslint-env jest */
+import type { ThingConfig } from '../../flowTypes';
+
+const clearUpdateInputData = require('./clearUpdateInputData');
+
+describe('clearUpdateInputData', () => {
+  test('should remove connect', () => {
+    const thingConfig: ThingConfig = {};
+    Object.assign(thingConfig, {
+      name: 'Example',
+      relationalFields: [
+        {
+          name: 'relationalField',
+          config: thingConfig,
+        },
+      ],
+      duplexFields: [
+        {
+          name: 'duplexField',
+          config: thingConfig,
+          oppositeName: 'duplexField',
+        },
+      ],
+    });
+
+    const data = {
+      relationalField: { connect: '5cefb33f05d6be4b7b59842b' },
+      duplexField: { connect: '5cefb33f05d6be4b7b59842c' },
+    };
+
+    const expectedResult = {
+      relationalField: '5cefb33f05d6be4b7b59842b',
+      duplexField: '5cefb33f05d6be4b7b59842c',
+    };
+
+    const result = clearUpdateInputData(data, thingConfig);
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('should return embedded array', () => {
+    const embeddedConfig: ThingConfig = {
+      name: 'Embedded',
+      embedded: true,
+      textFields: [
+        {
+          name: 'embeddedTextField',
+        },
+      ],
+    };
+
+    const exampleConfig: ThingConfig = {
+      name: 'Example',
+      textFields: [
+        {
+          name: 'textField',
+        },
+      ],
+      embeddedFields: [
+        {
+          name: 'embeddedFields',
+          config: embeddedConfig,
+          array: true,
+        },
+      ],
+    };
+
+    const data = {
+      textField: 'text Field',
+      embeddedFields: [
+        {
+          embeddedTextField: 'embedded Text Field 1',
+        },
+        {
+          embeddedTextField: 'embedded Text Field 2',
+        },
+      ],
+    };
+
+    const expectedResult = {
+      textField: 'text Field',
+      embeddedFields: [
+        {
+          embeddedTextField: 'embedded Text Field 1',
+        },
+        {
+          embeddedTextField: 'embedded Text Field 2',
+        },
+      ],
+    };
+
+    const result = clearUpdateInputData(data, exampleConfig);
+
+    expect(result).toEqual(expectedResult);
+  });
+});
