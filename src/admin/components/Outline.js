1da732ced87cb4144161a421494f7088aa7d4b02
Valentyn Soloviov <v.solovyov@intellect.ua>
2019-06-19 10:02:20 +0300

    style(src/admin/components/Outline.js): Update to remove console.log

--- src/admin/components/Outline.js
+++ src/admin/components/Outline.js
@@ -65,7 +65,6 @@ type Props = {
 
 const Outline = ({ children, error, message, label }: Props) => {
   const classes = useStyles();
-
   return (
     <React.Fragment>
       <fieldset aria-hidden className={clsx(classes.root, { [classes.error]: error })}>
