c3025d441f5dba539a1c15c9a43c4702424c0d53
Valentyn Soloviov <v.solovyov@intellect.ua>
2019-06-21 10:14:52 +0300

    build(src/index.js): Update to add client utils to index file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import composeGqlTypes from './types/composeGqlTypes';
 import composeGqlResolvers from './resolvers/composeGqlResolvers';
 import Admin from './admin/components/Admin';
+import composeMutation from './client/mutations/composeMutation';
+import composeQuery from './client/queries/composeQuery';
+import composeSubscription from './client/subscriptions/composeSubscription';
 
-module.exports = {
-  Admin,
+export default {
+  // server side
   composeGqlResolvers,
   composeGqlTypes,
+  // client side
+  composeMutation,
+  composeQuery,
+  composeSubscription,
+  // client side admin
+  Admin,
 };
