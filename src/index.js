d76a4b84325425b776fac81b31de3b8a86d12793
Valentyn Soloviov <v.solovyov@intellect.ua>
2019-06-25 23:20:39 +0300

    build(src/index.js): Update to add all mutations and queries

--- src/index.js
+++ src/index.js
@@ -1,13 +1,53 @@
 // @flow
 
+// -----------
 // server side
+// -----------
+
 export { default as composeGqlResolvers } from './resolvers/composeGqlResolvers';
 export { default as composeGqlTypes } from './types/composeGqlTypes';
 
+// mutation resolvers
+export {
+  default as createCreateManyThingsMutationResolver,
+} from './resolvers/mutations/createCreateManyThingsMutationResolver';
+export {
+  default as createCreateThingMutationResolver,
+} from './resolvers/mutations/createCreateThingMutationResolver';
+export {
+  default as createUpdateThingMutationResolver,
+} from './resolvers/mutations/createUpdateThingMutationResolver';
+export {
+  default as createDeleteThingMutationResolver,
+} from './resolvers/mutations/createDeleteThingMutationResolver';
+
+// query resolvers
+export {
+  default as createThingCountQueryResolver,
+} from './resolvers/queries/createThingCountQueryResolver';
+export { default as createThingQueryResolver } from './resolvers/queries/createThingQueryResolver';
+export {
+  default as createThingsQueryResolver,
+} from './resolvers/queries/createThingsQueryResolver';
+
+// subscription resolvers
+export {
+  default as createCreatedThingSubscriptionResolver,
+} from './resolvers/subscriptions/createCreatedThingSubscriptionResolver';
+export {
+  default as createUpdatedThingSubscriptionResolver,
+} from './resolvers/subscriptions/createUpdatedThingSubscriptionResolver';
+export {
+  default as createDeletedThingSubscriptionResolver,
+} from './resolvers/subscriptions/createDeletedThingSubscriptionResolver';
+
+// -----------
 // client side
+// -----------
+
 export { default as composeMutation } from './client/mutations/composeMutation';
 export { default as composeQuery } from './client/queries/composeQuery';
 export { default as composeSubscription } from './client/subscriptions/composeSubscription';
 
-// client side admin
+// admin
 export { default as Admin } from './admin/components/Admin';
