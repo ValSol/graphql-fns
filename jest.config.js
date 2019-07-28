3322ac8d95640417e07bcd192cf10fa08bebda8f
Valentyn Soloviov <v.solovyov@intellect.ua>
2019-07-28 13:10:03 +0300

    test(jest.config.js): Update to use moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-  testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+  },
 };
