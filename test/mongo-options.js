6cc6cc1462209a3f3c9e19538421ded29094a3a2
Valentyn Soloviov <v.solovyov@intellect.ua>
2021-09-12 14:16:15 +0300

    build: update mongoose version

--- test/mongo-options.js
+++ test/mongo-options.js
@@ -1,10 +1,6 @@
 const mongoOptions = {
-  poolSize: 5,
-  promiseLibrary: global.Promise,
   retryWrites: false,
-  useCreateIndex: true,
   useNewUrlParser: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
 };
 
