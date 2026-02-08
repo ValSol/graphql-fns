9439f7ad383ee7ed3651d0fc7f1ed44e826c8343
Valentyn Soloviov <v.solovyov@intellect.ua>
2026-02-08 11:21:31 +0200

    build(package.json): update all packages & add client exports

---
+++ src/client/index.ts
@@ -0,0 +1,10 @@
+import fromGlobalId from '@/resolvers/utils/fromGlobalId';
+import toGlobalId from '@/resolvers/utils/toGlobalId';
+
+// types
+
+export type * from '@/tsTypes';
+
+// export all
+
+export { fromGlobalId, toGlobalId };
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/client/index.ts'],
   outDir: 'dist',
   splitting: false,
   sourcemap: true,
   treeshake: true,
   clean: true,
-  dts: true,
+  dts: {
+    entry: ['src/index.ts', 'src/client/index.ts'],
+  },
   format: ['esm', 'cjs'],
   target: 'node22',
 });
